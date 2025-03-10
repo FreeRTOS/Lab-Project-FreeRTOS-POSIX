9fe6bfec80ba306b21799e747d4e5b14ed5b5eb1
Gaurav Aggarwal <aggarg@amazon.com>
2025-03-10 16:03:59 +0000

    Fix license header
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- test/posix_build_test/FreeRTOSConfig.h
+++ test/posix_build_test/FreeRTOSConfig.h
@@ -1,6 +1,6 @@
 /*
- * FreeRTOS V202212.00
- * Copyright (C) 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Amazon FreeRTOS POSIX V1.1.0
+ * Copyright (C) 2018 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -19,9 +19,8 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
- * https://www.FreeRTOS.org
- * https://github.com/FreeRTOS
- *
+ * http://aws.amazon.com/freertos
+ * http://www.FreeRTOS.org
  */
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
